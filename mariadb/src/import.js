dc5ea1eeef2e898ebad91c1b23cccfc55f819be6
bereket tesfaye <berekettesfaye7909@gmail.com>
2025-02-10 17:26:10 +0100

    queryed all the users in the database and displayed it in the console using console.table

--- mariadb/src/import.js
+++ mariadb/src/import.js
@@ -1,10 +1,11 @@
 import db from '../config/database.js';
+console.log( "hello "+process.env.DATABASE_PASSWORD)
 
 const con = await db.initialize();
 
-const sql = 'SELECT * FROM users WHERE `username` = ?';
-const args =[ "alice"]
+const sql = 'SELECT * FROM users';
+const args =[]
 const [results] = await con.execute(sql , args);
 
-console.log(results);
+console.table(results);
 await db.close();
